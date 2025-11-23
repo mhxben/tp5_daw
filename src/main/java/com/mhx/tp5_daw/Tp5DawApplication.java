f4f53c893c1ffb64d14d3d75b17544e116cef3ac
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2025-11-23 11:03:41 +0100

    Initial commit

---
+++ src/main/java/com/mhx/tp5_daw/Tp5DawApplication.java
@@ -0,0 +1,13 @@
+package com.mhx.tp5_daw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5DawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5DawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhx/tp5_daw/Tp5DawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhx.tp5_daw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5DawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
